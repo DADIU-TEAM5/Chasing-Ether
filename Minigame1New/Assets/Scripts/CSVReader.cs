1d1931d09ad760d3ae3a18bcd9c68c1580092958
Magnus <magnusmhp@gmail.com>
2019-09-05 11:03:49 +0200

    CSV stuff

--- Minigame1New/Assets/Scripts/CSVReader.cs
+++ Minigame1New/Assets/Scripts/CSVReader.cs
@@ -5,30 +5,50 @@ using UnityEngine;
 
 public class CSVReader : MonoBehaviour
 {
+
+    public Transform rokokoSkeleton;
+
+    public List<indexedBone> indexedBones;
+
+    public bool done = false;
     // Start is called before the first frame update
-    public struct Joint
-    {
-        public Vector3 position;
-        public Vector4 rotation;
-
-    }
-
-    public struct Frame
-    {
-        public Joint[] joints;
-        public int frameNumber;
-        public string animationName;
-
-
-    }
-
+    
     public List<Frame> animations = new List<Frame>(); 
     void Start()
     {
-        using (var reader = new StreamReader(@"C:\Users\dadiu\Documents\DADIU\Git\MiniGame1\MiniGame1\Assets\Animations\SuddenCrouch_DEFAULT_E46.csv"))
+        List<Transform> listOfAllBones = new List<Transform>();
+        indexedBones = new List<indexedBone>();
+
+        GetAllChildren(rokokoSkeleton, listOfAllBones);
+
+        
+
+        using (var reader = new StreamReader(@"C:\Users\dadiu\Documents\GitHub\MiniGame1_New\Minigame1New\Assets\Animations\StandingIdle_MIXAMO_E46.csv"))
         {
 
-            reader.ReadLine();
+            var nameLine= reader.ReadLine();
+            var boneName = nameLine.Split(',');
+
+            for (int i = 0; i < boneName.Length; i++)
+            {
+                for (int j = 0; j < listOfAllBones.Count; j++)
+                {
+                   if(boneName[i].Contains(listOfAllBones[j].name))
+                    {
+                        indexedBone bone = new indexedBone();
+                        bone.index = i/7 ;
+
+                        //print("bone with index " + bone.index + " created");
+                        bone.bone = listOfAllBones[j];
+                        indexedBones.Add(bone);
+                    }
+                }
+            }
+
+            
+            print(indexedBones[7].bone.name +" "+indexedBones[7].index);
+
+
             while (!reader.EndOfStream)
             {
                 Frame temp = new Frame();
@@ -36,35 +56,62 @@ public class CSVReader : MonoBehaviour
 
                 var line = reader.ReadLine();
                 var values = line.Split(',');
+                float[] floatValues = new float[values.Length];
 
-                //print(values.Length);
 
-                //print("values: " + values[14]);
-
-                float j = 0;
-                for (int i = 1; i < values.Length;)
+                // Convesion from string to float
+                for (int i = 0; i < values.Length; i++)
                 {
-                    //print("modulo "+ j % 2);
-                    if (i + 4 >= values.Length)
-                        break;
-                    if((int)j%2 == 1)
+
+                    if (values[i].Contains("."))
                     {
-                      //  print(values[i]);
-                        Vector3 tempvect = new Vector3(float.Parse(values[i]), float.Parse(values[i+1]), float.Parse(values[i+2]));
-                        temp.joints[(int)j].position = tempvect;
-                        //print(tempvect + " vector " + i);
-                        i += 3;
+                        int valueLength = values[i].Length;
+                        floatValues[i] = float.Parse(values[i]);
+
+                        int start = 0;
+                        if (values[i].Contains("-"))
+                        {
+                            start++;
+                        }
+
+                        for (int j = start; j < valueLength - 2; j++)
+                        {
+                            floatValues[i] *= 0.1f;
+                        }
+
                     }
                     else
                     {
-                        
-                        Vector4 tempvect = new Vector4(float.Parse( values[i]), float.Parse(values[i+1]), float.Parse(values[i+2]), float.Parse(values[i+3]));
-                        temp.joints[(int)j].rotation = tempvect;
-                        i += 4;
-                        //print("rotation " + values[i] + "Joint " + j + "frame " + i);
-                        
+                        floatValues[i] = float.Parse(values[i]);
                     }
-                    j+=0.5f;
+
+
+
+                    // Print single frame
+                    if (animations.Count == 18)
+                        print(values[i] + " , " + floatValues[i]);
+                }
+
+
+
+
+                for (int i = 1; i < values.Length - 8; i += 7)
+                {
+                    for (int j = 0; j < temp.joints.Length; j++)
+                    {
+
+
+
+                        Vector3 tempvect = new Vector3(floatValues[i], floatValues[i + 1], floatValues[i + 2]);
+                        temp.joints[j].position = tempvect;
+                        //print("Joint: " + j + " : " + tempvect);
+
+                        Vector4 tempvec4 = new Vector4(floatValues[i + 3], floatValues[i + 4], floatValues[i + 5], floatValues[i + 6]);
+                        temp.joints[j].rotation = tempvec4;
+
+
+
+                    }
 
                 }
 
@@ -72,18 +119,19 @@ public class CSVReader : MonoBehaviour
 
             }
 
-
+            /*
             for (int i = 0; i < animations[1].joints.Length; i++)
             {
                 print("Joint: " + i + " = position = " + animations[5].joints[i].position);
                 print("Joint: " + i + " rotatation = " + animations[5].joints[i].rotation);
 
             }
+            */
         }
 
 
-
-
+        print("Data Loaded");
+        done = true;
     }
 
     // Update is called once per frame
@@ -91,4 +139,44 @@ public class CSVReader : MonoBehaviour
     {
         
     }
+
+
+    void GetAllChildren(Transform parent, List<Transform> childList)
+    {
+        
+
+        if (parent.childCount > 0)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                childList.Add(parent.GetChild(i));
+                GetAllChildren(parent.GetChild(i), childList);
+            }
+        }
+        
+
+    }
 }
+
+
+public struct Joint
+{
+    public Vector3 position;
+    public Vector4 rotation;
+
+}
+
+public struct Frame
+{
+    public Joint[] joints;
+    public int frameNumber;
+    public string animationName;
+
+
+}
+public struct indexedBone
+{
+    public int index;
+    public Transform bone;
+}
+
---
+++ Minigame1New/Assets/Scripts/JointData.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointData : MonoBehaviour
+{
+
+    public CSVReader data;
+    //public Transform[] joints = new Transform[26];
+
+
+    List<Frame> animations;
+
+    List<indexedBone> idxBones;
+
+    public int FrameRate = 4;
+    public bool interpolate = true;
+
+
+    int clipLength;
+
+    float currentFrame = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    bool init = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (data.done)
+        {
+            if (init)
+            {
+                animations = data.animations;
+
+                idxBones = data.indexedBones;
+                init = false;
+
+
+
+                clipLength = animations.Count;
+                print("we got " + idxBones.Count + " bones");
+
+
+                getBoneWithIndex(50).rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+
+                
+            }
+            //print((int)currentFrame);
+            PlayAnimation();
+
+        }
+    }
+
+
+
+
+    void PlayAnimation()
+    {
+
+        currentFrame = (currentFrame + FrameRate * Time.deltaTime)% clipLength;
+
+        
+        if (interpolate)
+        {
+           
+            //ShoulderJoint.localRotation = Quaternion.Lerp(Quaternion.Euler(shoulderJointPos[(int)currentFrame]), Quaternion.Euler(shoulderJointPos[((int)currentFrame + 1) % clipLength]), currentFrame - (int)currentFrame);
+
+        }
+        else
+        {
+
+            //print("We got here");
+            for (int i = 0; i < animations[(int)currentFrame].joints.Length; i++)
+            {
+               getBoneWithIndex(i).rotation =vec4ToQuaternion( animations[(int)currentFrame].joints[i].rotation);
+               // getBoneWithIndex(i).position = animations[50].joints[i].position;
+
+            }
+            
+        }
+        
+
+    }
+
+    Quaternion vec4ToQuaternion(Vector4 vec)
+    {
+        print(vec);
+        Quaternion qt = new Quaternion(vec.x, vec.y, vec.z, vec.w);
+        return qt;
+    }
+
+    Transform getBoneWithIndex(int idx)
+    {
+        Transform tf;
+        for (int i = 0; i < idxBones.Count; i++)
+        {
+            if (idxBones[i].index == idx) {
+                tf = idxBones[i].bone;
+                return tf;
+                    }
+        }
+
+        Debug.LogError("no bone with index "+idx+" found");
+        return transform;
+    }
+}
