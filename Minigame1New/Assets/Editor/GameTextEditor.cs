737b890c9eb51212471cde8042ae2d5ad642267b
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-06 17:12:04 +0200

    almost finished, just need the editor for the gametext objects

---
+++ Minigame1New/Assets/Editor/GameTextEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameText))]
+public class GameTextEditor : Editor
+{
+    private Locale _newLocale;
+    private string _newText;
+    
+    private string _errorMessage;
+
+    private int _currentChosenLocaleText;
+
+    public override void OnInspectorGUI() {
+        
+        var gametextObj = (GameText) target;
+
+        if (gametextObj.TextVariations == null) {
+            gametextObj.TextVariations = new Dictionary<Locale, string>();
+        }
+
+        foreach(var localetext in gametextObj.TextVariations) {
+            GUILayout.Label(localetext.Key.name);
+            gametextObj.TextVariations[localetext.Key] = EditorGUILayout.TextArea(localetext.Value);
+        }
+
+
+        _newLocale = (Locale) EditorGUILayout.ObjectField(_newLocale, typeof(Locale), true);
+        _newText = EditorGUILayout.TextArea(_newText);
+
+        if (GUILayout.Button("Add locale text")) {
+            if (!gametextObj.TextVariations.ContainsKey(_newLocale) && _newText?.Length > 0) {
+                 gametextObj.TextVariations.Add(_newLocale, _newText);
+            }
+        }
+   }
+
+}
---
+++ Minigame1New/Assets/Scripts/ScriptableObjects/GameText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameText : ScriptableObject
+{
+    public Dictionary<Locale, string> TextVariations;
+
+    public LocaleVariable CurrentLocale; 
+
+    public string GetText() {
+        var text = "";
+
+        TextVariations.TryGetValue(CurrentLocale.Value, out text);
+
+        return text;
+    }
+}
