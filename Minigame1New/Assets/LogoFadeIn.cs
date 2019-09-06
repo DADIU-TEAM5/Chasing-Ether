a6727a3ac0052e6ecc6f19eef1ea1ad565ffd71d
Martin Krøll <martino1215rhotmail.com>
2019-09-06 16:47:07 +0200

    Menu background done

---
+++ Minigame1New/Assets/LogoFadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoFadeIn : MonoBehaviour
+{
+    public float speed;
+    private Vector3 downMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        downMove = new Vector3(0, speed * Time.deltaTime, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(transform.position);
+        if (transform.position.y > 150)
+        {
+            
+            transform.Translate(downMove);
+        }
+
+    }
+}
---
+++ Minigame1New/Assets/Scripts/MonoBehaviours/CloudMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMenuScript : MonoBehaviour
+{
+
+
+    public float rotationSpeed = 0.5f;
+    public float heightMinimum = 40;
+    public float heightMaximum = 50;
+
+    float currentRotationSpeed = 0;
+    public GameObject cloud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomHeight = Random.Range(heightMinimum, heightMaximum);
+        cloud.transform.position = new Vector3(transform.position.x, transform.position.y + randomHeight, transform.position.z + 500);
+
+        DefineRotationSpeed();
+        transform.Rotate(Vector3.up, Random.Range(0, 360));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, currentRotationSpeed, 0 * Time.deltaTime);
+    }
+
+    public void DefineRotationSpeed()
+    {
+        currentRotationSpeed = Random.Range(-rotationSpeed, rotationSpeed);
+
+        if (currentRotationSpeed < 0.01f && currentRotationSpeed > -0.01f)
+        {
+            DefineRotationSpeed();
+        }
+    }
+}
