2689dfc9f2648b3fff253a1eebc822b2f54f523b
Martin Krøll <martino1215rhotmail.com>
2019-09-09 14:46:52 +0200

    Menu is chaos

---
+++ Minigame1New/Assets/Scripts/SceneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    void Start()
+    {
+        //myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/scenes");
+        //scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startScene(int sceneNumber)
+    {
+
+        print("Enter scene");
+
+        if (sceneNumber == 1)
+        {
+            print("Loading LevelDesignScene_2");
+            SceneManager.LoadScene("LevelDesignScene_2", LoadSceneMode.Additive);
+
+            print("Changing to LevelDesignScene_2");
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("LevelDesignScene_2"));
+        }
+      
+       
+    }
+
+}
---
+++ Minigame1New/Assets/buttonHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class buttonHandler : MonoBehaviour
+{
+    public Button yourButton;
+    public RawImage image;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        print("You have clicked the button!");
+        image.GetComponent<Animator>().SetBool("pressedTitle", true);
+    }
+}
