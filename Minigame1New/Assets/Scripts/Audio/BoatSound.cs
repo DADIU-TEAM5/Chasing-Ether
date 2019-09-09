f2165b0ae22f3f0e88c0ac29d56b113b8533c16d
MikkelAnttila <42273448+MikkelAnttila@users.noreply.github.com>
2019-09-09 15:09:16 +0200

    Implemented more sounds, set up test scene

---
+++ Minigame1New/Assets/Scripts/Audio/BoatSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSound : MonoBehaviour
+{
+   
+
+    AK.Wwise.Event EngineLoop;
+    public FloatVariable Speed;
+
+    void Start()
+    {
+        EngineLoop.Post(gameObject);
+    }
+
+    void Update()
+    {
+        // Send speed value to Wwise, controlling engine pitch
+        AkSoundEngine.SetRTPCValue("BoatSpeed", Speed.Value);
+   
+    }
+}
