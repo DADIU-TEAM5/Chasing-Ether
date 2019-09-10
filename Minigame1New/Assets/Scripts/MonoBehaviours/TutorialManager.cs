3818509ebfd00e11693c685fd9cb227e5b5578fc
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-10 14:48:46 +0200

    Created the tutorialmanager (except its not for the tutorial HA)

---
+++ Minigame1New/Assets/Scripts/MonoBehaviours/TutorialManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GyroController Gyro;
+    public MicrophoneInput Microphone;
+
+    public GameEvent StartBlowUI;
+    public GameEvent EndBlowUI;
+
+    public GameEvent StartTiltUI;
+    public GameEvent EndTiltUI;
+
+    public string TutorialLevelName;
+
+    void Start() {
+        StartCoroutine(Tutorial(TutorialLevelName));
+    }
+
+
+    IEnumerator Tutorial(string tutorialLevelName) {
+        yield return new WaitForSeconds(2);
+
+        StartBlowUI.Raise();
+
+        yield return new WaitForSeconds(1);
+        if (!Microphone.soundIsActivated) {
+            yield return null;
+        }
+
+        EndBlowUI.Raise();
+        
+        yield return new WaitForSeconds(1);
+
+        StartTiltUI.Raise();
+
+        if (Gyro.steeringInput == 0f) {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1);  
+
+        EndTiltUI.Raise();
+
+        yield return new WaitForSeconds(1);  
+
+        SceneManager.LoadScene(tutorialLevelName);
+    }
+}
