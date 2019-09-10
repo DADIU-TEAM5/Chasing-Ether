b2c23562075bd5c9de06b20ff97cc28473db8ce1
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-10 12:11:22 +0200

    imported boss movement

--- Minigame1New/Assets/Scripts/MonoBehaviours/GolemBehaviour.cs
+++ Minigame1New/Assets/Scripts/MonoBehaviours/GolemBehaviour.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class GolemBehaviour : MonoBehaviour
 {
+
+    public GameObject finishLineObj;
+    public GameObject playerObj;
+    private Vector3 finishLinePos;
+    private Vector3 bossStartPos;
+    private Vector3 playerStartPos;
+    private Vector3 playerCurPos;
+    private float playerBossDist;
+    public float lerpSpeed = 2f;
+    private Vector3 tempSpeed = Vector3.zero;
+    public BoolVariable walkingAnim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bossStartPos = transform.position;
+        playerStartPos = playerObj.transform.position;
+        finishLinePos = new Vector3(finishLineObj.transform.position.x, bossStartPos.y, finishLineObj.transform.position.z);
+        playerBossDist = Mathf.Abs(bossStartPos.z - playerStartPos.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(walkingAnim.Value == true)
+        {
+            playerCurPos = playerObj.transform.position;
+
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(bossStartPos.x, bossStartPos.y, playerCurPos.z + playerBossDist), ref tempSpeed, lerpSpeed);
+        }
+    
+       
     }
 }
---
+++ Minigame1New/Assets/Scripts/MonoBehaviours/WalkCycle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkCycle : MonoBehaviour
+{
+    static Animator anim;
+    public BoolVariable walkingAnim;
+    public BoolVariable throwAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (walkingAnim.Value == true)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isThrowing", false);
+        }
+
+        if (throwAnim.Value == true)
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isThrowing", true);
+
+            walkingAnim.Value = false;
+        }
+
+        if (walkingAnim.Value == false && throwAnim.Value == false)
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isThrowing", false);
+        }
+   
+    }
+
+    public void ThrowAnimEnd()
+    {
+        walkingAnim.Value = true;
+        throwAnim.Value = false;
+    }
+
+}
