b8364b879c9507583a7970dea06661f71a578816
MikkelAnttila <42273448+MikkelAnttila@users.noreply.github.com>
2019-09-10 09:44:16 +0200

    The boat now has sound!
    Implemented boat sound to boat prefab

--- Minigame1New/Assets/Scripts/Audio/BoatSound.cs
+++ Minigame1New/Assets/Scripts/Audio/BoatSound.cs
@@ -8,18 +8,29 @@ public class BoatSound : MonoBehaviour
 
     public AK.Wwise.Event EngineLoop;
     public AK.Wwise.Event BoatTurn;
-    public FloatVariable Speed;
-    public FloatVariable TurnAngle;
+    public PlayerController Boat;
+    public GameObject BoatTransform;
+    //public FloatVariable Speed;
+    // public FloatVariable TurnAngle;
+    public float RotationSoundAngle;
+
 
     void Start()
     {
         EngineLoop.Post(gameObject);
+        BoatTurn.Post(gameObject);
     }
 
+   
     void Update()
     {
         // Send speed value to Wwise, controlling engine pitch
-        AkSoundEngine.SetRTPCValue("BoatSpeed", Speed.Value);
-        AkSoundEngine.SetRTPCValue("BoatTurnAngle", TurnAngle.Value);
+        // AkSoundEngine.SetRTPCValue("BoatSpeed", Speed.Value);
+
+        RotationSoundAngle = Mathf.Abs(BoatTransform.transform.localRotation.z * 100);
+        AkSoundEngine.SetRTPCValue("BoatTurnAngle", RotationSoundAngle);
+        AkSoundEngine.SetRTPCValue("BoatSpeed", Boat.velocity);
+
+
     }
 }
