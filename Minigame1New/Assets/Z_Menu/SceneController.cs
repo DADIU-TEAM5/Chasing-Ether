8b83f2b16f54ae931cc34ae8903623a8b61bfcdf
Martin Krøll <martino1215rhotmail.com>
2019-09-10 12:07:04 +0200

    real final stuff

---
+++ Minigame1New/Assets/Z_Menu/SceneController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+    public GameObject menuParrent;
+
+    void Start()
+    {
+        //myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/scenes");
+        //scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Called when a level is selected from the menu
+    // TODO Add the relevant levels
+    public void startScene(int sceneNumber)
+    {
+
+        print("Enter scene");
+
+        if (sceneNumber == 1)
+        {
+            print("Loading LevelDesignScene_2");
+            SceneManager.LoadScene("LevelDesignScene_2", LoadSceneMode.Additive);
+
+            Destroy(menuParrent);
+
+            print("Changing to LevelDesignScene_2");
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("LevelDesignScene_2"));
+
+           
+        }
+      
+       
+    }
+
+}
