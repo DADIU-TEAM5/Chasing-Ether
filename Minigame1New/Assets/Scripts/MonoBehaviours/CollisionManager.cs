76d511abb25549a3230a852a316b5204963ce626
nufanDK <t_a_p@hotmail.com>
2019-09-11 10:24:44 +0200

    skybox and golem textures

--- Minigame1New/Assets/Scripts/MonoBehaviours/CollisionManager.cs
+++ Minigame1New/Assets/Scripts/MonoBehaviours/CollisionManager.cs
@@ -9,6 +9,7 @@ public class CollisionManager : MonoBehaviour
     public IntVariable playerHealth;
     public BoolVariable death;
     public Vector3Variable respawnPoint;
+    public Vector3Variable respawnPointCam;
 
     public Vector3 respawnTemp;
     public int healthTemp;
@@ -16,6 +17,7 @@ public class CollisionManager : MonoBehaviour
     public GameEvent collisionEvent;
     
     public GameObject TeleportObject;
+    public GameObject camLookAtPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,7 @@ public class CollisionManager : MonoBehaviour
         playerHealth.Value = 5;
         
         respawnPoint.Value = transform.position;
+        respawnPointCam.Value = camLookAtPoint.transform.position;
     }
 
     void OnTriggerEnter(Collider collider)
@@ -57,6 +60,9 @@ public class CollisionManager : MonoBehaviour
         transform.localRotation = Quaternion.identity;
         GameObject.FindWithTag("boatSplash").GetComponent<ParticleSystem>().Play();
         TeleportObject.GetComponent<PlayerController>().velocity = TeleportObject.GetComponent<PlayerController>().MinSpeed;
+
+        camLookAtPoint.transform.position = respawnPoint.Value + camLookAtPoint.transform.localPosition;
+        camLookAtPoint.transform.rotation = Quaternion.identity;
     }
 
 }
